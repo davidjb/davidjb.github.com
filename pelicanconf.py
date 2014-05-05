778189aad11b440a0dfad19eb32a3934f0eacada
David\ Beitey <david@davidjb.com>
2014-05-05 12:20:03 +1000

    Update for Pelican 3.3

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,17 +4,19 @@
 import itertools
 import os
 
-PLUGINS = ['pelican.plugins.gravatar']
+PLUGIN_PATH = 'src/pelican-plugins'
+PLUGINS = ['gravatar']
 
 AUTHOR = u'davidjb'
 AUTHOR_EMAIL = u'qnivq@qnivqwo.pbz'.decode('rot13')
 SITENAME = u'DavidJB.com'
 SITEURL = 'http://davidjb.com'
-SITESUBTITLE = "Ramblings about Plone, Pyramid, Python, the web, Linux, roses and more, by David Beitey."
+SITESUBTITLE = "Ramblings about Plone, Pyramid, Python, the web, Linux, " \
+               "and more, by David Beitey."
 THEME = 'notmyidea'
 
 DISQUS_SITENAME = 'davidjb'
-#GITHUB_URL = "http://github.com/davidjb/"
+# GITHUB_URL = "http://github.com/davidjb/"
 GOOGLE_ANALYTICS = 'UA-24253455-1'
 TWITTER_USERNAME = 'davidjb_'
 
@@ -24,12 +26,12 @@ DISPLAY_CATEGORIES_ON_MENU = False
 FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2})-.*'
 NEWEST_FIRST_ARCHIVES = True
 
+# All files in the `extras` directory get picked up.
+directory = 'content/extras'
 STATIC_PATHS = ['images', 'files']
+STATIC_PATHS.extend([os.path.join('extras', filename)
+                     for filename in os.listdir(directory)])
 EXTRA_TEMPLATES_PATHS = ['templates']
-
-#All files in the `extras` directory get picked up.
-directory='content/extras'
-FILES_TO_COPY = [(os.path.join('extras', filename), filename) for filename in os.listdir(directory)]
 
 TIMEZONE = 'Australia/Queensland'
 TYPOGRIFY = True
@@ -39,28 +41,23 @@ DEFAULT_LANG = u'en'
 ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{slug}/'
 ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{slug}/index.html'
 
-#Save all pages exactly as they are recorded in their slug
+# Save all pages exactly as they are recorded in their slug
 PAGE_URL = '{slug}'
 PAGE_SAVE_AS = '{slug}'
 
 # Blogroll
 external = (('Planet Plone', 'http://planet.plone.org'),
-            ('Apple Insider', 'http://appleinsider.com/'),
-           )
+            ('Apple Insider', 'http://appleinsider.com/'))
 professional = (('jcu.me Research Porfolio', 'http://jcu.me'),
-                ('Latest coding activity', 'http://git.io/djb'),
-               )
-fun = (('XKCD', 'http://xckd.com'),
-       ('OzBargain', 'http://www.ozbargain.com.au') 
-      )
-LINKS = list(itertools.chain(*itertools.izip(external, professional, fun))) 
+                ('Latest coding activity', 'http://git.io/djb'))
+fun = (('XKCD', 'http://xckd.com'),)
+LINKS = list(itertools.chain(*itertools.izip(external, professional, fun)))
 
 # Social widget
 SOCIAL = (('GitHub', 'http://git.io/djb'),
           ('Twitter', 'http://twitter.com/davidjb_'),
           ('LinkedIn', 'http://linkedin.com/in/davidbeitey'),
           ('Facebook', 'http://facebook.com/david.beitey'),
-          ('Google+', 'https://plus.google.com/u/0/106527454335411502430'),
-         )
+          ('Google+', 'https://plus.google.com/u/0/106527454335411502430'))
 
 DEFAULT_PAGINATION = 10
